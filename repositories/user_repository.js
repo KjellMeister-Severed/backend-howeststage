f6b340290977dafc2592e36c64c05cad2d2fbd31
Adriaan De Saeger <adriaan.de.saeger@student.howest.be>
2021-11-11 14:41:55 +0100

    Add repository function to create a new user

--- repositories/user_repository.js
+++ repositories/user_repository.js
@@ -1,6 +1,20 @@
 const database = require("../services/database");
 const azureRepository = require("./azure_repository");
 
+async function createUser(id) {
+    return new Promise((resolve) => {
+        database.executeQuery((connection) => {
+            connection.query(`INSERT INTO users (id) VALUES (?)`,
+            [id],
+            async function (err) {
+                if (err) throw err;
+
+                resolve(true);
+            });
+        });
+    });
+}
+
 async function getUserById(id) {
     return new Promise((resolve) => {
         database.executeQuery((connection) => {
